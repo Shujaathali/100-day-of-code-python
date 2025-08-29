c0deb3b868823543dba640d4cd0992e3c7abdda8
Mohammad Shujaath Ali <mohammadshujaathali@yandex.com>
2025-08-29 23:37:33 +0530

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,5 @@
+# Day1
+
+print("hello world")
+print("My name is Mohammad Shujaath Ali and i am learning python")
+print("Today is 29 september and toady is my day one in 100 days of coding python journey")
