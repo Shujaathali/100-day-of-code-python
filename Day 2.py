296ebc6c7b8c2039807cdc159f6f25217e8cc82b
Mohammad Shujaath Ali <mohammadshujaathali@yandex.com>
2025-08-31 01:56:16 +0530

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,54 @@
+#this is a day 2 of 100 day of python coding 
+#today we are learning about comment,escape
+#sequence characters 
+
+
+
+#Now first one is what is comment and how we can use it
+
+#so comment means you write somthing in code but you wont python can't exicute this lines so you can use # hashtag and write any thing for example alert message, reminder etc whatever you want python can't execute so use # and "' '" triple quotetion like this
+
+print("hello world")# can't remove this line
+"'can't remove this line'"
+print("hello world")
+
+
+#And second one is escape sequence characters escape sequence character only use in string if you write outside of string so show's syntex error besauce ecsape sequence inside the  string
+
+# Escape sequences demo in Python
+
+print("1. New Line (\\n):")
+print("Hello\nWorld")
+
+print("\n2. Tab (\\t):")
+print("Hello\tWorld")
+
+print("\n3. Backslash (\\\\):")
+print("C:\\Users\\Shoaib")
+
+print("\n4. Single Quote (\\'):")
+print('It\'s OK')
+
+print("\n5. Double Quote (\\\"):")
+print("He said \"Hi\"")
+
+print("\n6. Carriage Return (\\r):")
+print("Hello\rWorld")
+
+print("\n7. Backspace (\\b):")
+print("Helloo\b")
+
+print("\n8. Form Feed (\\f):")
+print("Hello\fWorld")
+
+print("\n9. Bell Alert (\\a):")
+print("Beep sound if system supports\a")
+
+print("\n10. Vertical Tab (\\v):")
+print("Hello\vWorld")
+
+# Raw string demo
+print("\n11. Raw String (r'...'):")
+print(r"Hello\nWorld")
+
+#if you dont want to execute escape sequence characters so you can use this sequence print(r"write \nstring \fhere")
