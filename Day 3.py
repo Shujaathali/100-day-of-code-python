ad126efbde4349476125a2e38b6bde58e97e2b17
Mohammad Shujaath Ali <mohammadshujaathali@yandex.com>
2025-09-02 01:40:39 +0530

    Create Day 3.py

---
+++ Day 3.py
@@ -0,0 +1,45 @@
+# Now today we are learning how many builtin data types in python
+#There are 14 built in data types in python. In 14 data types only 5 data types has mutable and remaining 9 data types are immutable
+#1 Numerical
+#2 Text
+#3 Sequence
+#4 Set
+#5 Mapping
+#6 Boolean
+#7 binary
+
+'''1:-  Numerical
+Intiger   123456               Immutable
+Float  2.5 , 8.6               Immutable
+Complex number  4+6j  ,  5-9j  Immutable
+
+
+2:- Text
+String   "HELLO WORLD"         Immutable
+
+
+3:- Sequence 
+List     [3,4,5,6,7]           Mutable
+Truple    (2,3,4,5)            Immutable
+Range    Range(5)              Immutable
+
+
+
+4:-  Set
+
+Set    {2,3,4,5}                             Mutable
+Frozenset     frozenset{1,2,3} Immutable
+
+
+5:- Mapping
+Dictionary   {"a":1,"b":2}     Mutable
+
+6:-  Boolean
+Boolean   True, False          Immutable
+
+7:- Binary
+Bytes      b"abc"              Immutable                 
+Bytearray    bytearray(5)      Mutable
+memoryview  memoryview(b"abc") Mutable
+
+
