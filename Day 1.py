2f264cc02de02e6008f8f34f5c76e7cd9b7a3e45
Mohammad Shujaath Ali <mohammadshujaathali@yandex.com>
2025-09-02 08:38:59 +0530

    day01_hello_world.py

--- Day 1.py
+++ Day 1.py
@@ -2,7 +2,7 @@
 
 # This is my first Python program in the 100 Days of Code journey.
 
-print("Hello, world!")
+print("Hello, World!")
 print("My name is Mohammad Shujaath Ali and I am learning Python.")
 print("Today is 29 September and today is my Day 1 in the 100 Days of Coding journey.")
 
